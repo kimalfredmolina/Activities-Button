9b596a94948e8243c0d50d9db2b6f34ee12954e9
Yoznov <catalan.fritzjohn25@gmail.com>
2024-11-20 11:35:49 +0800

    Change the MainActivity.java and activity_main.xml back to hello world.

--- app/src/main/java/com/example/activitiesbutton/MainActivity.java
+++ app/src/main/java/com/example/activitiesbutton/MainActivity.java
@@ -1,73 +1,24 @@
 package com.example.activitiesbutton;
 
 import android.os.Bundle;
-import android.view.ContextMenu;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
-
-        TextView textView = findViewById(R.id.tv_context_menu);
-        registerForContextMenu(textView);
-
-        setTitle(R.string.appbar_title);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.appbar_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            Toast.makeText(this, getString(R.string.settings_selected), Toast.LENGTH_SHORT).show();
-            return true;
-        } else if (id == R.id.action_about) {
-            Toast.makeText(this, getString(R.string.about_selected), Toast.LENGTH_SHORT).show();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu(menu, v, menuInfo);
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.context_menu, menu);
-    }
-
-    @Override
-    public boolean onContextItemSelected(MenuItem item) {
-
-        int id = item.getItemId();
-        if (id == R.id.option_one) {
-            Toast.makeText(this, getString(R.string.option_one_selected), Toast.LENGTH_SHORT).show();
-            return true;
-        } else if (id == R.id.option_two) {
-            Toast.makeText(this, getString(R.string.option_two_selected), Toast.LENGTH_SHORT).show();
-            return true;
-        } else if (id == R.id.option_three) {
-            Toast.makeText(this, getString(R.string.option_three_selected), Toast.LENGTH_SHORT).show();
-            return true;
-        }
-        return super.onContextItemSelected(item);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }
